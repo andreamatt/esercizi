c591205213056fa0db869e23344c08d51b3b8fb2
andrea matte <30327952+andreamatt@users.noreply.github.com>
2017-10-12 23:12:11 +0200

    complete

--- 17-10-10/main.cpp
+++ 17-10-10/main.cpp
@@ -5,30 +5,135 @@
 using namespace std;
 
 int sum_N(vector<int>, int);
-int sum_DeI(vector<int>, int, int);
 int sommaseq_N_quad(vector<int> seq, int n);
+void sommaMat();
+void sum_N_NoSpace();
+void maxSubMatrixSum();
+int maxSubArraySum(int *array, int size);
 
 int main() {
-    vector<int> seq;
-    ifstream inputs("input.txt");
-    int len;
-    inputs>>len;
-    seq.resize(len);
-    for(int i=0; i<len; i++){
-        inputs>>seq[i];
-    }
 
-
-    ofstream out("output.txt");
-    out<<sum_N(seq, len);
-
+    maxSubMatrixSum();
 
 
     return 0;
 }
 
-int sum_DeI(vector<int> seq, int i, int j){
-    return 0;
+void sommaMat(){
+    ifstream input("input.txt");
+    int rows, cols;
+    input>>rows;
+    input>>cols;
+    vector<vector<int>> matrix;
+    matrix.resize(rows);
+    for (int i = 0; i < matrix.size(); i++) {
+        matrix[i].resize(cols);
+    }
+
+    int current;
+    for (int i = 0; i < rows; ++i) {
+        int tempsum = 0;
+        for (int j = 0; j < cols; ++j) {
+            input>>current;
+            tempsum += current;
+            matrix[i][j] = tempsum;
+            cout<<matrix[i][j]<<", ";
+        }
+        cout<<endl;
+    }
+
+    // calcul
+
+    for (int r = 0; r < rows; ++r) {
+        for (int c = 0; c < cols; ++c) {
+
+        }
+    }
+
+
+}
+
+
+void maxSubMatrixSum() {
+    int y;
+    int x;
+    int sum;
+    int maxSum = 0;
+    ifstream in("input.txt");
+    in >> y >> x;
+    int matrix[y][x];
+
+    // putting data into matrix
+    for (int k = 0; k < y; ++k) {
+        for (int i = 0; i < x; ++i) {
+            in >> matrix[k][i];
+        }
+    }
+
+    // main algorithm
+    for (int left = 0; left < x; left++) {
+        int temp[y] = {0};
+        for (int right = left; right < x; right++) {
+            for (int i = 0; i < y; ++i) {
+                temp[i] += matrix[i][right];
+            }
+            sum = maxSubArraySum(temp, y);
+            if (sum > maxSum)
+                maxSum = sum;
+        }
+    }
+    ofstream out("output.txt");
+    out << maxSum;
+}
+
+
+int maxSubArraySum(int *array, int size) {
+    int tmp = 0;
+    int tmpMax = 0;
+
+    for (int i = 0; i < size; ++i) {
+        tmp += array[i];
+        if (tmp < 0) {
+            tmp = 0;
+        }
+        tmpMax = max(tmpMax, tmp);
+    }
+    return tmpMax;
+}
+
+void sum_N_NoSpace(){
+    ifstream input("input.txt");
+    int n;
+    input>>n;
+    if(n == 0) {
+        fstream output("output.txt");
+        output<<0;
+    }
+    else {
+        int max_so_far, max_ending_here = 0;
+        int num;
+        input >> num;
+        max_so_far = num;
+        max_ending_here = max_ending_here + num;
+        if (max_so_far < max_ending_here)
+            max_so_far = max_ending_here;
+
+        if (max_ending_here < 0)
+            max_ending_here = 0;
+        for (int i = 1; i < n; i++) {
+            input >> num;
+            max_ending_here = max_ending_here + num;
+            if (max_so_far < max_ending_here)
+                max_so_far = max_ending_here;
+
+            if (max_ending_here < 0)
+                max_ending_here = 0;
+        }
+
+        // out
+        ofstream output("output.txt");
+        output<<max_so_far;
+    }
 }
 
 int max(int a, int b){
