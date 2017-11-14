fdbb95f9887d7c817ecb882fb7a0322ffab51ea7
andrea matte <30327952+andreamatt@users.noreply.github.com>
2017-11-14 14:38:23 +0100

    interv

--- intervalli/main.cpp
+++ intervalli/main.cpp
@@ -1,93 +1,223 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <time.h>
 
 using namespace std;
 
 void interval();
+void intervalSort();
+void merge(vector<pair<int,int>> &seq, int S_A, int E_A, int S_B, int E_B);
+void mergeSort(vector<pair<int,int>> &seq, int start, int end);
+
 
 int main() {
 
-    interval();
+    /*
+    srand(clock());
+    ofstream input("input.txt");
+    input<<200<<endl;
+    for(int i=0; i<200; i++){
+        int n = rand()%1000;
+        input<<n<<" "<<n + rand()%20<<endl;
+    }
+    */
+
+    intervalSort();
+    //interval();
 
     return 0;
 }
 
-void interval() {
+void intervalSort(){
     ifstream input("input.txt");
+    int size;
+    input>>size;
+
+    vector<pair<int,int>> intervals;
+    intervals.resize(size);
+    for(int i=0; i<size; i++){
+        input>>intervals[i].first;
+        input>>intervals[i].second;
+    }
+
+    mergeSort(intervals, 0, size-1);
+
+    // FIND MAX EMPTY INTER
+    //ofstream output2("output2.txt");
+    //output2<<size<<endl;
+
+    int start = 0;
+    int end = 0;
+    int maxEnding=0;
+    for(int i=0; i<size-1; i++){
+        //output2<<intervals[i].first<<" "<<intervals[i].second<<endl;
+        if(intervals[i].second > maxEnding){
+            maxEnding = intervals[i].second;
+        }
+        if(intervals[i+1].first - maxEnding > end-start){
+            start = maxEnding;
+            end = intervals[i+1].first;
+            //cout<<start<<" "<<end<<endl;
+        }
+    }
+
+    ofstream output("output.txt");
+    //output<<"Solution: "<<endl;
+    if(end-start == 0){
+        output<<"0";
+    }
+    else{
+        output<<start<<" "<<end;
+    }
+
+    //interval();
+}
+
+void mergeSort(vector<pair<int,int>> &seq, int start, int end){
+
+    if(end-start == 0){
+        return;
+    }
+    else if(end-start == 1){
+        if(seq[start] > seq[end]){
+            pair<int,int> t = seq[end];
+            seq[end].first = seq[start].first;
+            seq[end].second = seq[start].second;
+
+            seq[start].first = t.first;
+            seq[start].second = t.second;
+        }
+    }
+    else{
+        mergeSort(seq, start, (start+end)/2);
+        mergeSort(seq, (start+end)/2 +1, end);
+        merge(seq, start, (start+end)/2, (start+end)/2+1, end);
+    }
+
+}
+
+void merge(vector<pair<int,int>> &seq, int S_A, int E_A, int S_B, int E_B){
+
+    if(E_A +1 != S_B){
+        cout<<"Non consecutive"<<endl;
+        return;
+    }
+
+    vector<pair<int,int>> result;
+    result.resize(E_B-S_A+1);
+    int a=S_A, b=S_B, k=0;
+    while(a<=E_A && b<=E_B){
+        if(seq[a].first < seq[b].first){
+            result[k].first = seq[a].first;
+            result[k].second = seq[a].second;
+            a++;
+        }
+        else{
+            result[k].first = seq[b].first;
+            result[k].second = seq[b].second;
+            b++;
+        }
+        k++;
+    }
+
+    while(a <= E_A){
+        result[k].first = seq[a].first;
+        result[k].second = seq[a].second;
+        a++;
+        k++;
+    }
+
+    while(b <= E_B){
+        result[k].first = seq[b].first;
+        result[k].second = seq[b].second;
+        b++;
+        k++;
+    }
+
+    for(int i=0; i<result.size(); i++){
+        seq[S_A+i].first = result[i].first;
+        seq[S_A+i].second = result[i].second;
+    }
+
+}
+
+void interval() {
+    ifstream input("output2.txt");
     int size;
     input >> size;
 
-    pair<int, int> current;
+    int curFirst;
+    int curSecond;
     pair<int, int> limits;
     vector<pair<int, int>> emptyIntervals;
 
     // first one is manual
-    input >> current.first;
-    input >> current.second;
-    limits.first = current.first;
-    limits.second = current.second;
+    input >> curFirst;
+    input >> curSecond;
+    limits.first = curFirst;
+    limits.second = curSecond;
 
     for (int i = 1; i < size; i++) {
-        input >> current.first;
-        input >> current.second;
+        input >> curFirst;
+        input >> curSecond;
 
         // modify current emptyInters
         int currentIntervalsSize = emptyIntervals.size();
         for (int k = 0; k < currentIntervalsSize; k++) {
             // case 1: the k-inter is covered by i => delete k-inter
-            if (current.first <= emptyIntervals[k].first && current.second >= emptyIntervals[k].second) {
+            if (curFirst <= emptyIntervals[k].first && curSecond >= emptyIntervals[k].second) {
                 emptyIntervals.erase(emptyIntervals.begin() + k);
             }
 
                 // case 2: have to divide k-inter in 2 intervals
-            else if (current.first > emptyIntervals[k].first && current.second < emptyIntervals[k].second) {
+            else if (curFirst > emptyIntervals[k].first && curSecond < emptyIntervals[k].second) {
                 // right one
                 pair<int, int> rightInter;
-                rightInter.first = current.second;
+                rightInter.first = curSecond;
                 rightInter.second = emptyIntervals[k].second;
                 emptyIntervals.push_back(rightInter);
 
                 // left one
-                emptyIntervals[k].second = current.first;
+                emptyIntervals[k].second = curFirst;
             }
 
                 // case 3: first is inside
-            else if (current.first > emptyIntervals[k].first && current.first < emptyIntervals[k].second) {
-                emptyIntervals[k].second = current.first;
+            else if (curFirst > emptyIntervals[k].first && curFirst < emptyIntervals[k].second) {
+                emptyIntervals[k].second = curFirst;
             }
 
                 // case 4: second is inside
-            else if (current.second > emptyIntervals[k].first && current.second < emptyIntervals[k].second) {
-                emptyIntervals[k].first = current.second;
+            else if (curSecond > emptyIntervals[k].first && curSecond < emptyIntervals[k].second) {
+                emptyIntervals[k].first = curSecond;
             }
         }
 
         // add possible new-start and new-end inters
-        if (current.first < limits.first) {
-            if (current.second < limits.first) {
+        if (curFirst < limits.first) {
+            if (curSecond < limits.first) {
                 pair<int, int> left;
-                left.first = current.second;
+                left.first = curSecond;
                 left.second = limits.first;
                 emptyIntervals.push_back(left);
             }
 
-            limits.first = current.first;
+            limits.first = curFirst;
         }
-        if (current.second > limits.second) {
-            if (current.first > limits.second) {
+        if (curSecond > limits.second) {
+            if (curFirst > limits.second) {
                 pair<int, int> right;
                 right.first = limits.second;
-                right.second = current.first;
+                right.second = curFirst;
                 emptyIntervals.push_back(right);
             }
 
-            limits.second = current.second;
+            limits.second = curSecond;
         }
     }
 
     // END. NOW PRINT RESULT
-    ofstream output("output.txt");
+    ofstream output("output3.txt");
     if (emptyIntervals.size() == 0) {
         output << "0";
     }
