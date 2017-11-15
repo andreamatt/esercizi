aac528b058c6976a8218bb9b64f3bf6fdeab30c4
andrea matte <30327952+andreamatt@users.noreply.github.com>
2017-11-15 14:47:02 +0100

    diametro

--- diametro/main.cpp
+++ diametro/main.cpp
@@ -1,15 +1,68 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
 struct nodo{
-    vector<nodo> vic;
-    bool visited;
+    vector<nodo*> vic;
+    int value;
+    bool visited = false;
 };
 
-int main() {
+int maxFromHere(nodo*, bool);
 
+int main() {
+    int N, M;
+    vector<nodo> grafo;
+    ifstream input("input.txt");
+    input>>N>>M;
+    grafo.resize(N);
+    for(int i=0; i<N; i++){
+        grafo[i].value = i;
+    }
+    for(int i=0; i<M; i++){
+        int from, to;
+        input>>from>>to;
+        grafo[from].vic.push_back(&grafo[to]);
+        grafo[to].vic.push_back(&grafo[from]);
+    }
+
+    int maxD = 0;
+    bool valueW = true;
+    for(int i=0; i<N; i++){
+        maxD = max(maxD, maxFromHere(&grafo[i], valueW));
+        valueW = !valueW;
+    }
+
+    ofstream output("output.txt");
+    output<<maxD;
 
     return 0;
 }
+
+int maxFromHere(nodo* from, bool valueWant){
+
+    vector<pair<nodo*,int>> toReach;
+    from->visited = true;
+    pair<nodo*,int> p;
+    p.first = from;
+    p.second = 0;
+    toReach.push_back(p);
+    //cout<<"Distance from "<<from->value<<endl;
+    for(int i=0; i<toReach.size(); i++){
+        for(nodo* j : toReach[i].first->vic){
+            if(j->visited != valueWant){
+                pair<nodo*,int> t;
+                t.first = j;
+                t.second = toReach[i].second +1;
+                toReach.push_back(t);
+                j->visited = valueWant;
+                //cout<<"Added "<<t.first->value<<" "<<t.second<<endl;
+            }
+        }
+    }
+    //cout<<endl;
+    return toReach[toReach.size()-1].second;
+}
+
