a25bd1ab13849cd04e07541db9846197c26c27b1
Andrea Mattè <andreamatte97@gmail.com>
2018-03-01 15:12:49 +0100

    Moved vscode config
    working on knapsack

--- dinamic programming/knapsack/knapsack.cpp
+++ dinamic programming/knapsack/knapsack.cpp
@@ -2,48 +2,67 @@
 #include <climits>
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <unordered_map>
 
 using namespace std;
 
-int knapsackRec(vector<int> &weight, vector<int> &profit, int i, int c, int **DP) {
-	if (i == 0 || c == 0) {
-		return 0;
-	}
+struct pair_hash {
+    inline std::size_t operator()(const std::pair<int,int> & v) const {
+        return v.first*31+v.second;
+    }
+};
 
-	if (DP[i][c] < 0) {
-		int notTaken = knapsackRec(weight, profit, i - 1, c, DP);
-		int taken;
-		if(weight[i-1] <= c){
-			taken = knapsackRec(weight, profit, i - 1, c - weight[i-1], DP) + profit[i];
-		}
-		else{
-			taken = INT_MIN;
-		}
-		DP[i][c] = max(taken, notTaken);
-	}
-
-	return DP[i][c];
+int knapsackMemoHash(vector<int> w, vector<int> p, int n, int C){
+	unordered_map<pair<int,int>, int, pair_hash> DP;
+	DP.insert({make_pair(5, 2), 10});
+	cout<<DP.at(make_pair(5, 2));
+	return -1; 
 }
 
-int knapsack(vector<int> &weight, vector<int> &profit, int C) {
-	int items = weight.size();
-	int **DP = new int *[items + 1];
-	for (int i = 0; i < items + 1; i++) {
-		DP[i] = new int[C + 1];
-		for (int j = 0; j < C + 1; j++) {
-			DP[i][j] = -1;
+int knapsackIter(vector<int> w, vector<int> p, int n, int C){
+	int** DP = new int*[n+1];
+	for(int i=0; i<=n; i++){
+		DP[i] = new int[C+1]{};
+	}
+	
+	for(int i=1; i<=n; i++){
+		for(int c=1; c<=C; c++){
+			int notTaken = DP[i-1][c];
+			int taken = (w[i] <= c) ? DP[i-1][c - w[i]] + p[i] : -1;
+			DP[i][c] = max(taken, notTaken);
 		}
 	}
 	
-	return knapsackRec(weight, profit, items, C, DP);
+	for(int i=0; i<=n; i++){
+		for(int c=0; c<=C; c++){
+			cout<<DP[i][c]<<", ";
+		}
+		cout<<endl;
+	}
+	
+	return DP[n][C];
 }
 
 int main() {
-	vector<int> weight = {5, 24, 5, 23, 14, 35};
-	vector<int> profit = {21, 32, 1, 21, 12, 14};
-	int C = 50;
 
-	cout << knapsack(weight, profit, C);
+	int C;
+	int n;
+	ifstream input("input.txt");
+	input>>C>>n;
+	vector<int> weight(n+1);
+	vector<int> profit(n+1);
+	weight.push_back(-1);
+	profit.push_back(-1);
+
+	for(int i=1; i<=n; i++){
+		input>>weight[i];
+		input>>profit[i];
+	}
+
+	int result = knapsackMemoHash(weight, profit, n, C);
+	ofstream output("output.txt");
+	output<<result;
 
 	return 0;
 }
